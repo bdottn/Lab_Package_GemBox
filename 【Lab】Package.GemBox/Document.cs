667e3ab5638b838b04e9cd8f13f4c2d1f3427924
Echo Lee <bdottn.lee+git@gmail.com>
2025-03-17 14:58:22 +0800

    建立類別：Document，實作 HelloWorld

---
+++ 【Lab】Package.GemBox/Document.cs
@@ -0,0 +1,28 @@
+﻿using GemBox.Document;
+
+namespace Lab.Package.GemBox
+{
+    class Document
+    {
+        private const string LicenseKey = "FREE-LIMITED-KEY";
+
+        public void HelloWorld()
+        {
+            // If using the Professional version, put your serial key below.
+            ComponentInfo.SetLicense(LicenseKey);
+
+            DocumentModel document = new DocumentModel();
+
+            Section section = new Section(document);
+            document.Sections.Add(section);
+
+            Paragraph paragraph = new Paragraph(document);
+            section.Blocks.Add(paragraph);
+
+            Run run = new Run(document, "Hello World!");
+            paragraph.Inlines.Add(run);
+
+            document.Save("HelloWorld.docx");
+        }
+    }
+}
